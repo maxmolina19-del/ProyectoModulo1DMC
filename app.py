b49e24f4b6ca55be4408be6a8bbb682b92042031
maxmolina19-del <maxmolina19@gmail.com>
2026-07-19 18:26:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ import streamlit as st
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
-valor_inicial = st.number_input("Ingrese el valor inicial")
+valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
+valor_final = st.number_input("Ingrese el valor final",value=1)
+
+lista_numerica = list(range(valor_inicial,valor_final))
