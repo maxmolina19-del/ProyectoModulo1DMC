29f855dec826cce3e6e5229f20ea75ad4aa8a6d0
maxmolina19-del <maxmolina19@gmail.com>
2026-07-19 18:39:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
+st.image("Python_logo.png")
+
 valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
 valor_final = st.number_input("Ingrese el valor final",value=1)
 
