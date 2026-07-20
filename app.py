6753cecc7df3935419ce16a496d528216790d867
maxmolina19-del <maxmolina19@gmail.com>
2026-07-19 19:02:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
 st.image("Python_logo.png")
-st.sidebar.image("DMC.png")
+st.sidebar.image("DMC.png") 
 
 valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
 valor_final = st.number_input("Ingrese el valor final",value=1)
