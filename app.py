d736ac8c530795234e3af5f9fdd586da8b41dc0b
maxmolina19-del <maxmolina19@gmail.com>
2026-07-19 19:12:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import numpy as np
+
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
@@ -9,9 +11,27 @@ modulo = st.sidebar.selectbox("Elija un módulo", ["Módulo Listas","Módulo Arr
 
 if modulo == "Módulo Listas":
   
-valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
-valor_final = st.number_input("Ingrese el valor final",value=1)
+  valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
+  valor_final = st.number_input("Ingrese el valor final",value=1)
+  
+  lista_numerica = list(range(valor_inicial,valor_final))
+  
+  st.write(lista_numerica)
 
-lista_numerica = list(range(valor_inicial,valor_final))
+elif modulo == "Módulo Array":
 
-st.write(lista_numerica)
+  st.write("Estas en el módulo de arreglos")
+  limite_inferior = st.number_input("Ingrese el límite inferior", value=1200)
+  limite_superior = st.number_input("Ingrese el límite superior", value = 1250)
+  cantidad_datos =  st.number_input("Ingrese totalidad de datos a crear", value = 31)
+
+  datos_produccion = np.random.randint(limite_inferior, limite_superior, cantidad_datos)
+
+  st.write(datos_produccion)
+
+  st.write("La producción total es:" ,  np.sum(datos_produccion))
+  st.write("La producción promedio es:" , np.mean(datos_produccion) )
+
+else:
+
+   st.write("Estas en el módulo de funciones")
